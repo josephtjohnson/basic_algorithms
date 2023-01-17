bb36c860aec73d1b6e43db5aa962b65a855bc4a7
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2023-01-17 12:52:45 -0500

    Add docstrings and adjust formatting

--- basic_algorithms/Task3/Task3.py
+++ basic_algorithms/Task3/Task3.py
@@ -1,10 +1,14 @@
-
-
-#Helper functions to sort the array 
 from array import ArrayType
 
-
 def findPivot(array, lowIndex, highIndex):
+    """
+    Find the pivot within the array.
+    Args:
+       input_list(array), lowIndex(int), highIndex(int): Input array to search and between
+       low and high indices.
+    Returns:
+       int: index for array pivot or -1
+    """
 
     pivot = array[highIndex]
 
@@ -17,12 +21,21 @@ def findPivot(array, lowIndex, highIndex):
 
             (array[i], array[j]) = (array[j], array[i])
 
-
     (array[i + 1], array[highIndex]) = (array[highIndex], array[i + 1])
 
     return i + 1
 
+
 def quick_sort(array, lowIndex, highIndex):
+    """
+    Performs a quick sort algorithm on an array recursively.
+    Args:
+       array(array), lowIndex(int), highIndex(int): Input array to search and between
+       low and high indices.
+    Returns:
+       array: sorted array
+    """
+
     if lowIndex < highIndex:
 
         pivot = findPivot(array, lowIndex, highIndex)
@@ -34,16 +47,25 @@ def quick_sort(array, lowIndex, highIndex):
     return [array]
 
 
-def rearrangeArray (array):
+def rearrangeArray(array):
+    """
+    Rearranges the array to be only two numbers who's size is not different by more than
+    one decimal place. The sum of the two numbers equals the max for combinations of numbers
+    under these conditions.
+    Args:
+       array(array): Input array to rearrange.
+    Returns:
+       array: array consisting of two numbers
+    """
 
     sorted_array = quick_sort(array, 0, len(array) - 1)
 
-    first_number = ''
-    second_number = ''
+    first_number = ""
+    second_number = ""
 
-    for i in range(len(array) -1, 0, -2):
+    for i in range(len(array) - 1, 0, -2):
         first_number += str(sorted_array[0][i])
-        second_number += str(sorted_array[0][i-1])
+        second_number += str(sorted_array[0][i - 1])
     first_number += str(sorted_array[0][0])
     final_array = [int(first_number), int(second_number)]
     return final_array
@@ -57,10 +79,10 @@ def test_function(test_case):
     else:
         print("Fail")
 
+
 test_function([[1, 2, 3, 4, 5], [542, 31]])
 test_case = [[4, 6, 2, 5, 9, 8], [964, 852]]
 
 test_function([[1, 2, 3, 4, 5], [542, 31]])
 
 test_function([[8, 0, 7, 1, 3], [831, 70]])
-
